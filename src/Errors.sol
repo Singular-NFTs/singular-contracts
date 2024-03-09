195ac5556b768dd8e0d3c345ca941e92ec22f96f
edsonalcala <edson_alcala@msn.com>
2024-03-09 12:36:29 -0600

    v0 with erc1155

---
+++ src/Errors.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+error CollectionInProgress();
+
+error MaxCollectionsReached();
+
+error InsufficientFee();
+
+error CollectionRequestAlreadyApproved();
+
+error CollectionRequestNotFound();
+
+error CollectionNotInProgress();
+
+error MaxItemsReachedForCollection();
+
+error InvalidCollection();
+
+error ItemRequestAlreadyApproved();
+
+error ItemRequestNotFound();
+
+error CollectionNotActive();
